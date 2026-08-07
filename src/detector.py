b751f2d475f499c95c227006dd2a80c38336fe3a
kakasam93-tech <kakasam93@gmail.com>
2026-08-07 05:06:08 +0300

    Create detector.py

---
+++ src/detector.py
@@ -0,0 +1,15 @@
+BASE_URL = "https://bou.or.ug"
+
+FINANCIAL_MARKETS_URL = "https://bou.or.ug/financial_market/"
+
+KEYWORDS = [
+    "Treasury Bond",
+    "Invitation to Tender",
+    "Government Treasury Bond",
+    "Re-opening",
+    "Reopening"
+]
+
+DOWNLOAD_FOLDER = "downloads"
+
+DATA_FOLDER = "data"
