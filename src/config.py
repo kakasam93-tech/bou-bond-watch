fa289d10195c2ad394bfbf8004e10890fbeb8138
kakasam93-tech <kakasam93@gmail.com>
2026-08-07 05:12:10 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# Bank of Uganda Bond Watch configuration
+
+BASE_URL = "https://bou.or.ug"
+
+FINANCIAL_MARKETS_URL = "https://bou.or.ug/financial_market/"
+
+KEYWORDS = [
+    "treasury bond",
+    "treasury bonds",
+    "invitation to tender",
+    "government treasury bond",
+    "re-opening",
+    "reopening",
+]
+
+DOWNLOAD_FOLDER = "downloads"
+
+DATA_FOLDER = "data"
